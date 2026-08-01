ad89bd9915e1537c8cfa3c7fc97b86edb1d339ed
Nicholas Palceski <128099643+NickPalceski@users.noreply.github.com>
2026-08-01 12:32:42 -0400

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        i = 1
+        nums = set(nums)
+
+        while i in nums:
+            i += 1
+        
+        return i
+
+        
