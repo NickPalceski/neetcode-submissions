cbcb8c85759605641d21f850f55f7328510bcccf
Nicholas Palceski <128099643+NickPalceski@users.noreply.github.com>
2026-09-15 17:28:53 -0400

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-1

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        # built in dict collections for hashable objects: {a : 5, b : 2, c : 1}
+        count = Counter(s)
+        maxOdd, minEven = 0, len(s)
+
+        for cnt in count.values():
+            # bitwise odd check - can be faster than modulo
+            if cnt & 1:
+                maxOdd = max(maxOdd, cnt)
+            else:
+                minEven = min(minEven, cnt)
+        
+        return maxOdd - minEven
